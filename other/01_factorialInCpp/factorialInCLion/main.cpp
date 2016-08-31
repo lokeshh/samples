2f24cd058a9e381ca49d95d09c39d74fc040d277
Omar Shaikh <omarasifshaikh@hotmail.com>
2016-08-31 15:47:56 -0700

    adding 02_sizeof and 03_vectors

--- other/01_factorialInCpp/factorialInCLion/main.cpp
+++ other/01_factorialInCpp/factorialInCLion/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "factorial.h"
+using namespace std;
 int main() {
   int n = 4;
-  std::cout << "factorial(" << n << "): " << factorial(n) << std::endl;
+  cout << "factorial(" << n << "): " << factorial(n) << endl;
   return 0;
 }
